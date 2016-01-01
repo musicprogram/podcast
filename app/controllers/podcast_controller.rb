3eb7038f8dc5fe52e1f884879989112e1c55811d
musicprogram <jahenao17@misena.edu.co>
2016-01-01 18:54:14 -0500

    vista show podcast

--- app/controllers/podcast_controller.rb
+++ app/controllers/podcast_controller.rb
@@ -2,4 +2,7 @@ class PodcastController < ApplicationController
   def index
   	@fondo_pod = true
   end
+  def show
+  	@fondo_pod_show = true
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'podcast/index'
+  get 'podcast/show'
 
   get 'homepage/index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
